3a47bcc9194dff826a4854dfa22ad0691181eda1
Eduardo GM <eduardo.floyd@outlook.com>
2023-05-28 17:05:56 -0300

    Projetos usando DynamoDB Local

---
+++ dynamo-local-data/src/main/java/com/binha/demo/model/Funcionario.java
@@ -0,0 +1,46 @@
+package com.binha.demo.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConvertedJson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamoDBTable(tableName = "funcionario")
+public class Funcionario {
+
+    @DynamoDBHashKey
+    private String funcionarioId;
+
+    @DynamoDBAttribute
+    private String nome;
+
+    @DynamoDBAttribute
+    @DynamoDBTypeConvertedJson(targetType = Departamento.class)
+    private Departamento departamento;
+
+    public String getFuncionarioId() {
+        return funcionarioId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
