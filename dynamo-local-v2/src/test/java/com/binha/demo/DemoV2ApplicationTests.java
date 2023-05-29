18e466359a3a8a8bca379c411782de4e79e71b4a
Eduardo GM <eduardo.floyd@outlook.com>
2023-05-29 17:23:51 -0300

    Nova estrutura de pastas

---
+++ dynamo-local-v2/src/test/java/com/binha/demo/DemoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
